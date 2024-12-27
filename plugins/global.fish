9cbfb310fc0a75ef3633d1ac18cc19887016018f
Mathieu Hardy <mhardy2008@gmail.com>
2024-12-27 13:36:02 +0100

    First commit

---
+++ plugins/global.fish
@@ -0,0 +1,39 @@
+function global_init
+    # Abbreviations
+    abbr fr       'find . -name "*.xml" -exec sed -i "s#toto#tata#g" "{}" \;'
+    abbr fs       'find . -name "*.xml" -exec egrep -niH --color "toto" "{}" \;'
+
+    abbr j        'jobs'
+    abbr pg       'ps aux | grep'
+
+    abbr c        'clear'
+    abbr e        'exit'
+
+    abbr g        'rg --smart-case --no-heading --sort path'
+    abbr grep     'egrep'
+
+    abbr l        'lsd --group-directories-first -h -l'
+    abbr m        'lsd --group-directories-first -h -l'
+    abbr ù        'lsd --group-directories-first -h -l'
+    abbr la       'lsd --group-directories-first -h -l -a'
+
+    abbr mkdir    'mkdir -p'
+
+    abbr t        'tail -f'
+    abbr make     'make -j4'
+    abbr h        'history'
+
+    abbr tree     'lsd --group-directories-first --git --tree -h -l'
+
+    abbr untar    'tar xvf'
+
+    abbr ee       'nvim'
+    abbr see      'sudo -E nvim'
+
+    abbr rm       'trash'
+
+    abbr todo     'rg --smart-case --no-heading --sort path TODO'
+
+    abbr reboot   'echo "No way !"'
+    abbr poweroff 'echo "No way !"'
+end
