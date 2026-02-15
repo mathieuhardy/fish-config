6a2b5ea265760506baad16341ee60df48494e5a7
Mathieu Hardy <mhardy2008@gmail.com>
2026-02-15 16:12:07 +0100

    Add NixOS commands

---
+++ plugins/nix.fish
@@ -0,0 +1,7 @@
+function nix_init
+    # Abbreviations
+    abbr ns 'sudo nixos-rebuild switch --flake .#nixos'
+    abbr ni 'nix-env -i'
+    abbr nr 'nix run nixpkgs#'
+    abbr nc 'nix-channel --list'
+end
